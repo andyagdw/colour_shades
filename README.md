a5b0321df63422323468eb18d547152b100a51f3
Andy Agyei-Dwumah <andydwumah@gmail.com>
2024-08-02 18:37:58 +0100

    Add all files

---
+++ README.md
@@ -0,0 +1,34 @@
+# Interactive Art 💥
+
+This project is a web-based tool that allows users to generate and interact with different
+shades of colours. Initially part of a university coding task, it has been improved upon to
+include interactive features and enhanced functionality.
+
+## Features ✨
+
+**Colour Selection**: Choose from three primary colours: Red, Green, or Blue<br>
+**Shade Quantity**: Select a number from 1 to 10 from a dropdown menu to determine how many shades
+of the selected colour will be displayed<br>
+**Interactive Divs**:
+* Randomly swap the position of two divs
+* Resize Divs:
+    * Increase the width of a div by left-clicking on it
+    * Decrease the width of a div by right-clicking on it
+
+## Getting started ✅
+
+Follow these simple steps to get started with the game:
+
+1. Clone the repository to your local machine: <br />
+   `git clone`
+
+2. Navigate to the project directory: <br />
+   `cd `
+
+3. Open `` with live server.
+
+## Credits
+### Contact
+
+If you have any questions or just want to connect, you can reach me on
+[LinkedIn](https://www.linkedin.com/in/andyagyeidwumah/) 👍.
---
+++ interactive_art.js
@@ -0,0 +1,134 @@
+/////////////////////////
+let roundNum = Math.round;
+let randomNum = Math.random;
+
+function randomInt(min, max) {  // function used to generate different shades of colours
+    min = Math.ceil(70);
+    max = Math.floor(255);
+    return Math.floor(randomNum()*(max-min+1) + min);
+}
+
+// function that displays the users selected choice (e.g., number of boxes they selected and colour of the boxes)
+function displaySelectedNumOfBoxes() {
+    // store the value of the number of boxes
+    let dropDownValue = document.getElementById("numbers").value;
+    // store the colour value
+    let colourValue = document.forms[0].elements.colours.value;
+
+if (colourValue === "Red") {
+    // Creating and style divs until I get to the selected number of boxes
+    for (let i = 0; i < dropDownValue; i++) {
+        let parent = document.getElementById('allDivs');
+        let child = document.createElement('div');
+        let myDiv = parent.appendChild(child);
+        myDiv.style.border = "1px solid #000";
+        myDiv.style.marginTop = "1.5em";
+        myDiv.style.height = "5vw";
+        myDiv.style.width = "5vw";
+        myDiv.style.float = "left";
+        myDiv.style.backgroundColor = 'rgb(' + randomInt() + ',' + roundNum(randomNum()*15) + ',' + roundNum(randomNum()*15) + ')';
+        myDiv.setAttribute('oncontextmenu', 'shrinkDivWidth(this)');
+        myDiv.setAttribute('onclick', 'increaseDivWidth(this)');
+    }
+} else if (colourValue === "Green") {
+    for (let i = 0; i < dropDownValue; i++) {
+        let parent = document.getElementById('allDivs');
+        let child = document.createElement('div');
+        let myDiv = parent.appendChild(child);
+        myDiv.style.border = "1px solid #000";
+        myDiv.style.marginTop = "1.5em";
+        myDiv.style.height = "5vw";
+        myDiv.style.width = "5vw";
+        myDiv.style.float = "left";
+        myDiv.style.backgroundColor = 'rgb(' + roundNum(randomNum()*50) + ',' + randomInt() + ',' + roundNum(randomNum()*50) + ')';
+        myDiv.setAttribute('oncontextmenu', 'shrinkDivWidth(this)');
+        myDiv.setAttribute('onclick', 'increaseDivWidth(this)');        
+    } 
+} else if (colourValue === "Blue") {
+    for (let i = 0; i < dropDownValue; i++) {
+        let parent = document.getElementById('allDivs');
+        let child = document.createElement('div');
+        let myDiv = parent.appendChild(child);
+        myDiv.style.border = "1px solid #000";
+        myDiv.style.marginTop = "1.5em";
+        myDiv.style.height = "5vw";
+        myDiv.style.width = "5vw";
+        myDiv.style.float = "left";
+        myDiv.style.backgroundColor = 'rgb(' + roundNum(randomNum()*5) + ',' + roundNum(randomNum()*5) + ',' + randomInt() + ')';
+        myDiv.setAttribute('oncontextmenu', 'shrinkDivWidth(this)');
+        myDiv.setAttribute('onclick', 'increaseDivWidth(this)');
+    }
+}
+}
+
+const submitButton = document.querySelector(".myButton");
+submitButton.addEventListener("click", displaySelectedNumOfBoxes);
+
+/////////////////////////
+function shrinkDivWidth(selectedDiv) {  // shrink the width of the div when user right clicks on it
+    let currentWidth = selectedDiv.style.width;
+    let a2 = currentWidth.replace(/vw/gi, '');  // get rid of measurements to turn current width into a number
+    let a3 = a2 / 2;
+    let a4 = a3 + "vw";  // Add measurement
+    selectedDiv.style.width = a4;
+}
+
+function increaseDivWidth(selectedDiv) {  // increase the width of the div when the user clicks on it
+    let currentWidth = selectedDiv.style.width;
+    let a2 = currentWidth.replace(/vw/gi, '');
+    let a3 = a2 * 2;
+    let a4 = a3 + "vw";
+    selectedDiv.style.width = a4;
+}
+
+/////////////////////////
+const body = document.getElementById('my-body');
+
+body.addEventListener('keydown', function(event) { // swap two random divs when user presses 's'
+    if (event.code == 'KeyS') {
+        let lengthOfDivs = document.getElementById('allDivs').children.length;
+        let a1 = Math.floor(randomNum()*lengthOfDivs);
+        let a2 = Math.floor(randomNum()*lengthOfDivs);
+        let div1 = document.getElementById('allDivs').getElementsByTagName('div')[a1];
+        let div2 = document.getElementById('allDivs').getElementsByTagName('div')[a2];
+        let div1colour = div1.style.backgroundColor;
+        div1.style.backgroundColor = div2.style.backgroundColor;
+        div2.style.backgroundColor = div1colour;
+    }});
+
+/////////////////////////
+// Icons
+const moon = '<i class="fa-regular fa-moon"></i>';
+const sun = '<i class="fa-regular fa-sun"></i>';
+const icons = [sun, moon];
+
+// Elements
+const h1 = document.querySelector(".header-h1");
+const textInfo = document.querySelector(".info");
+const selectLabel = document.querySelector(".select-label");
+const btnThemeToggler = document.querySelector(".btn-theme-toggler");
+
+function changeElementColours() {
+  if (body.classList.contains("bodyWhite")) {
+    body.classList.replace("bodyWhite", "bodyBlack");
+    textInfo.classList.replace("blackColour", "whiteColour");
+    h1.classList.replace("blackColour", "whiteColour");
+    selectLabel.classList.replace("blackColour", "whiteColour");
+    btnThemeToggler.innerHTML = icons[0];
+  } else {
+      body.classList.replace("bodyBlack", "bodyWhite");
+      textInfo.classList.replace("whiteColour", "blackColour");
+      h1.classList.replace("whiteColour", "blackColour");
+      selectLabel.classList.replace("whiteColour", "blackColour");
+      btnThemeToggler.innerHTML = icons[1];
+  }
+}
+
+const buttonThemeToggler = document.querySelector(".btn-theme-toggler");
+buttonThemeToggler.addEventListener("click", changeElementColours);
+
+/////////////////////////
+const form = document.getElementById("my-form");
+form.addEventListener("submit", e => {  // prevent form from refreshing on submit
+    e.preventDefault();
+})
